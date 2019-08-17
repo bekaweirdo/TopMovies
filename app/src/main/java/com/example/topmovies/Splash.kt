64a9b94fc2918465a9bd480d7892c0fb309fb047
bekka <beqachachua@gmail.com>
2019-08-17 11:26:26 +0400

    create handler for running splash screen 3 seconds

--- app/src/main/java/com/example/topmovies/Splash.kt
+++ app/src/main/java/com/example/topmovies/Splash.kt
@@ -1,12 +1,36 @@
 package com.example.topmovies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class Splash : AppCompatActivity() {
+    private var mDelayHanlder: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 // 3 seconds
 
+    internal val mRunnable: Runnable = Runnable {
+        if(!isFinishing){
+            val intent = Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //For initialize Handler
+        mDelayHanlder = Handler()
+
+        //Navigate with delay
+        mDelayHanlder!!.postDelayed(mRunnable,SPLASH_DELAY)
+    }
+
+    override fun onDestroy() {
+        if(mDelayHanlder != null){
+            mDelayHanlder!!.removeCallbacks(mRunnable)
+        }
+        super.onDestroy()
     }
 }
