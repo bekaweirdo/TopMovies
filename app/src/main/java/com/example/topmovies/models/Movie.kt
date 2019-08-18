cfceecbecaf4bfaf2c2f2c8ab8a526fa97bc980b
bekka <beqachachua@gmail.com>
2019-08-18 19:40:37 +0400

    create movie model and moviesresponse for api

--- app/src/main/java/com/example/topmovies/models/Movie.kt
+++ app/src/main/java/com/example/topmovies/models/Movie.kt
@@ -1,2 +1,18 @@
 package com.example.topmovies.models
 
+import com.google.gson.annotations.SerializedName
+
+data class Movie (
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("overview")
+    val overview: String,
+    @SerializedName("original_title")
+    val originalTitle: String,
+    @SerializedName("vote_average")
+    val rating: String,
+    @SerializedName("release_date")
+    val releaseDate: String
+)
--- app/src/main/java/com/example/topmovies/models/MoviesResponse.kt
+++ app/src/main/java/com/example/topmovies/models/MoviesResponse.kt
@@ -1,2 +1,8 @@
 package com.example.topmovies.models
 
+import com.google.gson.annotations.SerializedName
+
+data class MoviesResponse (
+    @SerializedName("results")
+    val results: ArrayList<Movie>
+)
