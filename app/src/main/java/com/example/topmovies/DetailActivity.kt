6406c4b8fa3b2fe978b9e10e30427aa6d396b1f5
bekka <beqachachua@gmail.com>
2019-08-19 19:33:13 +0400

    add room database insert function and intent get extra strings

--- app/src/main/java/com/example/topmovies/DetailActivity.kt
+++ app/src/main/java/com/example/topmovies/DetailActivity.kt
@@ -1,8 +1,21 @@
 package com.example.topmovies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toolbar
+import androidx.room.Room
+import com.bumptech.glide.Glide
+import com.example.topmovies.database.AppDB
+import com.example.topmovies.database.EmpEntity
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,7 +23,57 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val imageView = findViewById<ImageView>(R.id.poster)
+        val title = findViewById<TextView>(R.id.title)
+        val overview = findViewById<TextView>(R.id.overview)
+        val originalTitle = findViewById<TextView>(R.id.originalTitle)
+        val rating = findViewById<TextView>(R.id.rating)
+        val release = findViewById<TextView>(R.id.release)
 
+
+        val thumbnail: String = getIntent().getStringExtra("poster")
+        val movieName: String = getIntent().getStringExtra("title")
+        val plot: String = getIntent().getStringExtra("overview")
+        val releaseDate: String = getIntent().getStringExtra("release")
+        val original: String = getIntent().getStringExtra("original_title")
+        val rate: String = getIntent().getStringExtra("rating")
+
+        Glide.with(this)
+            .load(thumbnail)
+            .placeholder(R.drawable.loading)
+            .into(imageView)
+        title.text = movieName
+        overview.text = plot
+        release.text = releaseDate
+        rating.text = rate
+        originalTitle.text = original
+
+
+        //Database
+        var db = Room.databaseBuilder(applicationContext, AppDB::class.java, "MovieDB").allowMainThreadQueries().build()
+
+        val FabButton = findViewById<FloatingActionButton>(R.id.favouriteButton)
+        FabButton.setOnClickListener {
+            var movieDb = EmpEntity()
+            movieDb.empTitle = movieName
+            movieDb.empOverView = plot
+            movieDb.empOriginalTitle = original
+            movieDb.empRelease = releaseDate
+            movieDb.empRating = rate
+            movieDb.empPosterPath = thumbnail
+
+            db.empDAO().saveAll(movieDb)
+            Snackbar.make(it, "Movie added Favourite lists", Snackbar.LENGTH_LONG).setAction("Action", null)
+                .show()
+        }
+
+        //initCollapseToolbar()
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
